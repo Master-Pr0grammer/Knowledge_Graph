59fa8012a0bad5e137d530e96512ddb3fc5c32d3
Daniel-Hee <hed@rpi.edu>
2023-11-05 11:58:56 -0500

    Presentation Version

--- backend/categorization.py
+++ backend/categorization.py
@@ -46,10 +46,10 @@ After this step, for the example input, your output might look like this:
 
 ```
 {
-    'What is known as the powerhouse of the cell?': ['biology', 'parts of the cell'],
-    'What is the part of the cell that contains genetic information?': ['biology', 'organelles'],
-    'What is mRNA?': ['biology', 'genetics'],
-    'What is a good definition of overfitting?': ['machine learning', 'model training']
+    "What is known as the powerhouse of the cell?": ["biology", "parts of the cell"],
+    "What is the part of the cell that contains genetic information?": ["biology", "organelles"],
+    "What is mRNA?": ["biology", "genetics"],
+    "What is a good definition of overfitting?": ["machine learning", "model training"]
 }
 ```
 
@@ -68,14 +68,16 @@ After this final step, you might have produced something that looks like this:
 
 ```
 {
-    'What is known as the powerhouse of the cell?': ['biology', 'cell biology'],
-    'What is the part of the cell that contains genetic information?': ['biology', 'cell biology'],
-    'What is mRNA?': ['biology', 'genetics'],
-    'What is a good definition of overfitting?': ['machine learning', 'model training']
+    "What is known as the powerhouse of the cell?": ["biology", "cell biology"],
+    "What is the part of the cell that contains genetic information?": ["biology", "cell biology"],
+    "What is mRNA?": ["biology", "genetics"],
+    "What is a good definition of overfitting?": ["machine learning", "model training"]
 }
 ```
 
-Note you also output in JSON form."""
+Note you also output in JSON form.
+Note that all strings are encapsulated in double quotes.
+"""
 
 def remove_duplicate_categories(result_dict):
     most_recent_categories = {}
